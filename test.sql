1faba3a2e7ea8fdd19aaa249bf65bb642b508591
lngomy <lyciango8686@gmail.com>
2023-11-25 14:53:24 +1100

    init new repo

---
+++ test.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE TABLE rpt_order AS (
+    SELECT
+        order_id,
+        state,
+        item_count,
+        revenue,
+        discount,
+        shipping_cost
+
+    FROM f_orders
+    JOIN d_customers
+        ON orders.c_id = customers.id
+)
